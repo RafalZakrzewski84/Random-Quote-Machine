05be2e4c6937a059964aab7067f1cf8d6f50a272
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-09-28 22:42:24 +0200

    Adding packages and configuring webpack

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world</h1>);
+root.render(<h1 id="quote-box">Hello, world</h1>);
